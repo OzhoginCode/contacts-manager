9fec633d3c4d0c12d811338d2e0fa84c426c7343
Sandro <delta.f.alexrose@yandex.ru>
2024-06-18 20:41:41 +0300

    Логика для генерации паролей

---
+++ client/js/utils/passwordUtils.js
@@ -0,0 +1,16 @@
+// Функция для генерации случайного пароля
+function generatePassword() {
+  const length = 8;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
+
+// Экспорт функции генерации пароля
+module.exports = {
+  generatePassword
+};
